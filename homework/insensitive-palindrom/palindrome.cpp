f25ece307d41c5b803f27c35673cf909fdcf5403
Spror <wiktorspringer27@gmail.com>
2024-10-01 22:31:39 +0200

    feat(palindrome): implement is_palindrome function

--- homework/insensitive-palindrom/palindrome.cpp
+++ homework/insensitive-palindrom/palindrome.cpp
@@ -1,7 +1,16 @@
 #include "palindrome.hpp"
 #include <algorithm>
+#include <iostream>
 
-bool is_palindrome(std::string str)
-{
-    return true;
+bool is_palindrome(std::string str) {
+    str.erase(std::remove_if(begin(str), end(str), [](const auto& x) {
+                  return !std::isalnum(x);
+              }),
+              end(str));
+
+    std::transform(begin(str), end(str), begin(str), [](auto& ch) {
+        return std::tolower(ch);
+    });
+
+    return std::equal(begin(str), end(str), str.rbegin(), str.rend());
 }
