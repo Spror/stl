24a15a62744bedc99524621d6a57b752c065cf77
Spror <wiktorspringer27@gmail.com>
2024-10-02 00:17:00 +0200

    feat(decompressGrayscale): implementation of decompressGrayscale

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -32,5 +32,20 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array
 
 std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> image_v)
 {
-    
+    std::array<std::array<uint8_t, width>, height> decompress_image;
+    auto count = 0;
+    auto row = 0;
+
+    std::for_each(image_v.begin(), image_v.end(), [&](std::pair<uint8_t, uint8_t> information){
+        for(size_t i = count; i < count + static_cast<int>(information.second); i++)
+        {
+            decompress_image[count][row] = information.first;
+            if(count % width == 0)
+            {
+                row++;
+            }
+        }
+    });
+
+    return decompress_image;
 }
