b387357d0186bf6d6cb147fd30e22688f22b3bbd
Spror <wiktorspringer27@gmail.com>
2024-10-01 23:04:34 +0200

    feat(decompressGrayscale): start implementation of decompressGrayscale

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -29,3 +29,8 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array
 
     return compressed_image;
 }
+
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> image_v)
+{
+    
+}
