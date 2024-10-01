5c2a9d0ef378c7838194bad926d4184cb86313cd
Spror <wiktorspringer27@gmail.com>
2024-10-01 23:01:37 +0200

    feat(compressGrayscale): implement compressGrayscale

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -1,6 +1,31 @@
 #include "compression.hpp"
+#include <algorithm>
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> image_a)
 {
-    
+    std::vector<std::pair<uint8_t, uint8_t>> compressed_image;
+
+    for(const auto & row : image_a)
+    {
+        uint8_t current_value = row[0];
+        uint8_t count = 0;
+
+        std::for_each(row.begin(), row.end(), [&](auto pixel){
+            if(pixel == current_value)
+            {
+                count++;
+            }
+            else
+            {
+                compressed_image.emplace_back(current_value, count);
+                count = 1;
+                current_value = pixel;
+            }
+            
+        });
+
+        compressed_image.emplace_back(current_value, count);
+    }
+
+    return compressed_image;
 }
