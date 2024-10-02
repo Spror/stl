f96f239d155888dbda076f33b9a67e1d3759f6be
Spror <wiktorspringer27@gmail.com>
2024-10-02 02:11:01 +0200

    feat(removeDuplicateAndTranformToMap): start implementation

---
+++ homework/transform-containers/transformContainers.cpp
@@ -0,0 +1,5 @@
+#include "transformContainers.hpp"
+#include "algorithm"
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> str_l, std::list<std::string> deq) {
+}
--- homework/transform-containers/transformContainers.hpp
+++ homework/transform-containers/transformContainers.hpp
@@ -1,2 +1,7 @@
 #pragma once
+#include <deque>
+#include <list>
+#include <map>
+#include <string>
 
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> str_l, std::list<std::string> deq);
