7ba67b2c6e22dfc50138169452e64e5178c2aeca
Spror <wiktorspringer27@gmail.com>
2024-10-07 01:18:10 +0200

    feat(ArithmeticAverage): complete implementation

--- homework/inner-product/arithmeticAverage.cpp
+++ homework/inner-product/arithmeticAverage.cpp
@@ -1,5 +1,10 @@
 #include "arithmeticAverage.hpp"
+#include <numeric>
 
 double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second) {
+    if (first.empty() && second.empty()) {
+        return 0.0;
+    }
 
+    return (std::reduce(begin(first), end(first)) + std::reduce(begin(second), end(second))) / ((double)(first.size() + second.size()));
 }
