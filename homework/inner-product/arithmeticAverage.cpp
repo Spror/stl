64019eb470bac6f572b7437669d58f78e7b47d29
Spror <wiktorspringer27@gmail.com>
2024-10-07 01:38:31 +0200

    feat(Distance): complete implementation

--- homework/inner-product/arithmeticAverage.cpp
+++ homework/inner-product/arithmeticAverage.cpp
@@ -1,4 +1,7 @@
 #include "arithmeticAverage.hpp"
+#include <cmath>
+#include <functional>
+#include <iostream>
 #include <numeric>
 
 double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second) {
@@ -9,6 +12,18 @@ double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& 
     return (std::reduce(begin(first), end(first)) + std::reduce(begin(second), end(second))) / ((double)(first.size() + second.size()));
 }
 
-double Distance(const std::vector<int>& first, const std::vector<int>& second){
+double Distance(const std::vector<int>& first, const std::vector<int>& second) {
+    if (first.size() != second.size()) {
+        throw std::invalid_argument("Vectors must have the same size");
+    }
 
+    double sum_of_squares = std::transform_reduce(begin(first), end(first),
+                                                    begin(second),
+                                                    0,
+                                                    std::plus<>(),
+                                                    [](const auto& x, const auto& y) {
+                                                        return pow((x - y), 2);
+                                                    });
+
+    return std::sqrt(sum_of_squares);
 }
