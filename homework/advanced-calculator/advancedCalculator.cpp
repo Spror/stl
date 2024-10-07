20b324a919b7b2db12a5b19185b39fcf7bca9bfa
Spror <wiktorspringer27@gmail.com>
2024-10-07 19:55:03 +0200

    feat(process): add isValid function to check if input is valid

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -1,6 +1,26 @@
 #include "advancedCalculator.hpp"
+#include <algorithm>
+#include <cctype>
+#include <unordered_set>
 
-ErrorCode process(std::string input, double* out)
-{
-    return ErrorCode::OK;
+bool isValid(std::string input) {
+    static const std::unordered_set<char> valid_signs{'+', '-', '/', '*', '%', '!', '^', '$', '.'};
+
+    auto invalid = std::any_of(begin(input), end(input),
+                               [](const auto& ch) {
+                                   return !std::isdigit(ch) && !valid_signs.contains(ch);
+                               });
+
+    return !invalid;
+}
+
+ErrorCode process(std::string input, double* out) {
+    ErrorCode code;
+
+    if (isValid(input)) {
+        code = ErrorCode::BadCharacter;
+        return code;
+    }
+
+    return code;
 }
