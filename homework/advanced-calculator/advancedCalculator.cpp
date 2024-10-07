aab06b2ae72fd60afb8feeb502065d401d00ea3b
Spror <wiktorspringer27@gmail.com>
2024-10-07 21:05:22 +0200

    feat(process): rename function isValid to containsInvalidCharacter for clarity

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -3,7 +3,7 @@
 #include <cctype>
 #include <unordered_set>
 
-bool isValid(std::string input) {
+bool containsInvalidCharacter(std::string input) {
     static const std::unordered_set<char> valid_signs{'+', '-', '/', '*', '%', '!', '^', '$', '.'};
 
     auto invalid = std::any_of(begin(input), end(input),
@@ -11,13 +11,15 @@ bool isValid(std::string input) {
                                    return !std::isdigit(ch) && !valid_signs.contains(ch);
                                });
 
-    return !invalid;
+    return invalid;
 }
 
 ErrorCode process(std::string input, double* out) {
     ErrorCode code;
+    double first, second;
+    char op;
 
-    if (isValid(input)) {
+    if (containsInvalidCharacter(input)) {
         code = ErrorCode::BadCharacter;
         return code;
     }
