aa4c91b5191b4b79fe38c5ceafdea2385ecdbf30
Spror <wiktorspringer27@gmail.com>
2024-10-07 16:26:37 +0200

    feat(ErrorCode): create enum class for calculator error handling

---
+++ homework/advanced-calculator/advancedCalculator.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+enum class ErrorCode {
+    OK,
+    BadCharacter,
+    BadFormat,
+    DivideBy0,
+    SqrtOfNegativeNumber,
+    ModuleOfNonIntegerValue
+};
+
