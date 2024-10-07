dc53cf8d03111d0a0b486935a36d92a8b0865c3a
Spror <wiktorspringer27@gmail.com>
2024-10-07 21:55:06 +0200

    feat(process): add implementation of containsInvalidFormat function

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -1,6 +1,8 @@
 #include "advancedCalculator.hpp"
 #include <algorithm>
 #include <cctype>
+#include <iostream>
+#include <iterator>
 #include <unordered_set>
 
 bool containsInvalidCharacter(std::string input) {
@@ -8,7 +10,31 @@ bool containsInvalidCharacter(std::string input) {
 
     auto invalid = std::any_of(begin(input), end(input),
                                [](const auto& ch) {
-                                   return !std::isdigit(ch) && !valid_signs.contains(ch);
+                                   return !(std::isdigit(ch) || valid_signs.contains(ch));
+                               });
+
+    return invalid;
+}
+
+bool containsInvalidFormat(std::string input, int* distance) {
+    static const std::unordered_set<char> valid_operators{'+', '-', '/', '*', '%', '!', '^', '$'};
+    std::string first_str, second_str;
+
+    auto midle = std::find_if(begin(input), end(input),
+                              [](const auto& ch) {
+                                  return valid_operators.contains(ch);
+                              });
+
+    *distance = std::distance(begin(input), midle);
+
+    std::rotate(begin(input), midle, end(input));
+
+    auto invalid = std::any_of(begin(input) + 1, end(input),
+                               [count(0)](const auto& ch) mutable {
+                                   if (ch == '.')
+                                       count++;
+
+                                   return !(std::isdigit(ch) || std::isblank(ch)) || count > 2;
                                });
 
     return invalid;
@@ -16,13 +42,20 @@ bool containsInvalidCharacter(std::string input) {
 
 ErrorCode process(std::string input, double* out) {
     ErrorCode code;
-    double first, second;
-    char op;
+    int distance;
 
     if (containsInvalidCharacter(input)) {
         code = ErrorCode::BadCharacter;
         return code;
     }
 
-    return code;
+    if (containsInvalidFormat(input, &distance) || distance == 0) {
+        code = ErrorCode::BadFormat;
+        return code;
+    }
+
+    char op = input[distance];
+    //auto first = std::stod();
+
+    return ErrorCode::OK;
 }
