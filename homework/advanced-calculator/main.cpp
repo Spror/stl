df3b531d1df261ec8c7e706dbd179480b9a5ae1f
Spror <wiktorspringer27@gmail.com>
2024-10-08 15:53:31 +0200

    feat(main): implement user interaction loop

--- homework/advanced-calculator/main.cpp
+++ homework/advanced-calculator/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 #include "advancedCalculator.hpp"
 
+std::string handleErrorCode(ErrorCode code) {
+    switch (code) {
+    case ErrorCode::OK:
+        return "OK";
+        break;
+    case ErrorCode::BadCharacter:
+        return "BadCharacter";
+        break;
+    case ErrorCode::BadFormat:
+        return "BadFormat";
+        break;
+    case ErrorCode::DivideBy0:
+        return "DivideBy0";
+        break;
+    case ErrorCode::ModuleOfNonIntegerValue:
+        return "ModuleOfNonIntegerValue";
+        break;
+    case ErrorCode::SqrtOfNegativeNumber:
+        return "SqrtOfNegativeNumber";
+        break;
+    default:
+        return "Unknown Error";
+        break;
+    }
+}
+
 int main() {
-    std::cout << "hello world \n";
+    bool end_program = false;
+    double result;
+    ErrorCode code;
+    std::string input;
+
+    while (!end_program) {
+        std::getline(std::cin, input);
+        if (input == "q") {
+            end_program = true;
+        } else if ((code = process(input, &result)) == ErrorCode::OK) {
+            std::cout << "Result: " << result << std::endl;
+        } else {
+            std::cout << handleErrorCode(code) << std::endl;
+        }
+    }
 }
